a6f6a465f2241a17006b285d91da9f4cb71d2c9a
luanpoppe <luanpoppe@gmail.com>
2024-09-05 00:08:06 -0300

    feat: adicionando forma específica de lidar com revisão de 02 dias

--- back-end revisoes/utils/manage_data.py
+++ back-end revisoes/utils/manage_data.py
@@ -20,9 +20,22 @@ def formatDate(data):
 
 
 def adicionarRevisao(intervaloDaRevisao: int):
-  if (intervaloDaRevisao < 3):
+  if (intervaloDaRevisao < 2):
     return formatDate(daysFromToday(intervaloDaRevisao))
   else:
+    # Caso o intervalo seja 2 dias, as únicas possibilidades serão ou em 2 dias, ou em 1 dia, mas não poderá ser em 03 dias
+    if (intervaloDaRevisao == 2):
+      datas = [formatDate(daysFromToday(1)).split("-"), formatDate(daysFromToday(2)).split("-")]
+      umDia = datetime(year=int(datas[0][0]), month=int(datas[0][1]), day=int(datas[0][2])).date()
+      doisDias = datetime(year=int(datas[1][0]), month=int(datas[1][1]), day=int(datas[1][2])).date()
+
+      revisoesEmUmDia = RevisoesModel.objects.filter(proxima_data=umDia)
+      revisoesEmDoisDias = RevisoesModel.objects.filter(proxima_data=doisDias)
+      if len(revisoesEmDoisDias) > len(revisoesEmUmDia):
+        return umDia
+      else:
+        return doisDias
+
     # valores baseados no intervalo de 3 dias
     maximo = math.ceil(intervaloDaRevisao * 1.1) #4
     diferencaIntervaloMax = maximo - intervaloDaRevisao #1
