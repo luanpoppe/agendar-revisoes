079976c66e4a9ffa02be5ccf35be35fa54bfeefe
luanpoppe <luanpoppe@gmail.com>
2024-09-13 22:56:30 -0300

    feat: adicionando swagger básico

--- back-end revisoes/setup/settings.py
+++ back-end revisoes/setup/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django_filters",
     "revisoes",
     "corsheaders",
+    "drf_yasg"
 ]
 
 MIDDLEWARE = [
--- back-end revisoes/setup/urls.py
+++ back-end revisoes/setup/urls.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 
 from revisoes.views import revisoesHojeView, revisoesView, updateReview, pequenasRevisoesView, updatePequenasRevisoes, pequenasRevisoesHojeView
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Swagger API testando",
+        default_version="1.0.0",
+        description="Swagger de revisões"
+    ),
+    public=True
+)
+
 urlpatterns = [
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="swagger-schema"),
     path('admin/', admin.site.urls),
     path('revisoes/', revisoesView),
     path("revisoes-hoje/", revisoesHojeView),
