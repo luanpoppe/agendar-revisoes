2a13546ffe3cf6ac74bdf7a0053d0e9f251c49e7
luanpoppe <luanpoppe@gmail.com>
2024-11-03 18:33:32 -0300

    fix: endpoint correto da api em produção

--- front-end-revisoes/src/components/Card.tsx
+++ front-end-revisoes/src/components/Card.tsx
@@ -1,22 +1,29 @@
 import axios from "axios";
 import { Botao } from "./Botao";
+import { apiUrl } from "../environments";
 
 /* eslint-disable react/prop-types */
-export function Card(props: { revisao: Revisao, isRevisaoPequena?: boolean }) {
+export function Card(props: { revisao: Revisao; isRevisaoPequena?: boolean }) {
   function revisarCard() {
-    console.log("ENTROU AQUI")
-    const input = document.getElementById(props.isRevisaoPequena ? `input-revisao-pequena-${props.revisao.id}` : `input-${props.revisao.id}`) as HTMLInputElement
-    console.log('input: ', input)
+    console.log("ENTROU AQUI");
+    const input = document.getElementById(
+      props.isRevisaoPequena
+        ? `input-revisao-pequena-${props.revisao.id}`
+        : `input-${props.revisao.id}`
+    ) as HTMLInputElement;
+    console.log("input: ", input);
     if (input.value) {
-      props.revisao.intervalo_revisao = parseInt(input.value)
-      props.revisao.proxima_data = undefined
-      console.log('props.revisao: ', props.revisao)
-      const url = props.isRevisaoPequena ? `http://localhost:8000/revisao-pequena/${props.revisao.id}` : `http://localhost:8000/revisao/${props.revisao.id}`
-      console.log('url: ', url)
+      props.revisao.intervalo_revisao = parseInt(input.value);
+      props.revisao.proxima_data = undefined;
+      console.log("props.revisao: ", props.revisao);
+      const url = props.isRevisaoPequena
+        ? `${apiUrl}/revisao-pequena/${props.revisao.id}`
+        : `${apiUrl}/revisao/${props.revisao.id}`;
+      console.log("url: ", url);
       axios.patch(url, props.revisao).then((res) => {
-        const botaoFazerGet = document.getElementById("fazer-get")
-        botaoFazerGet.click()
-      })
+        const botaoFazerGet = document.getElementById("fazer-get");
+        botaoFazerGet.click();
+      });
     }
   }
 
@@ -46,9 +53,28 @@ export function Card(props: { revisao: Revisao, isRevisaoPequena?: boolean }) {
             Link do Notion
           </a>
           <div className="justify-content-center mt-3">
-            <label className="mb-1" htmlFor={props.isRevisaoPequena ? `input-revisao-pequena-${props.revisao.id}` : `input-${props.revisao.id}`}>Intervalo próxima Revisão: </label>
-            <input className="mb-3" type="number" id={props.isRevisaoPequena ? `input-revisao-pequena-${props.revisao.id}` : `input-${props.revisao.id}`}/>
-            <Botao color="primary" clicarBotao={revisarCard}>Revisado Hoje</Botao>
+            <label
+              className="mb-1"
+              htmlFor={
+                props.isRevisaoPequena
+                  ? `input-revisao-pequena-${props.revisao.id}`
+                  : `input-${props.revisao.id}`
+              }
+            >
+              Intervalo próxima Revisão:{" "}
+            </label>
+            <input
+              className="mb-3"
+              type="number"
+              id={
+                props.isRevisaoPequena
+                  ? `input-revisao-pequena-${props.revisao.id}`
+                  : `input-${props.revisao.id}`
+              }
+            />
+            <Botao color="primary" clicarBotao={revisarCard}>
+              Revisado Hoje
+            </Botao>
           </div>
         </div>
       </div>
