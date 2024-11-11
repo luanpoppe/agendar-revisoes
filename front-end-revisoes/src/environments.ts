1d613ccf52a8c54328f0fba2f0368de5b54f3906
luanpoppe <luanpoppe@gmail.com>
2024-11-11 09:17:04 -0300

    fix: baseUrl do backend para a vercel

--- front-end-revisoes/src/environments.ts
+++ front-end-revisoes/src/environments.ts
@@ -1,3 +1,3 @@
 // export const apiUrl = "https://agendar-revisoes.onrender.com";
 export const apiUrl =
-  "https://agendar-revisoes-git-luan-sozinho-luanpoppes-projects.vercel.app/";
+  "https://agendar-revisoes-git-luan-sozinho-luanpoppes-projects.vercel.app";
