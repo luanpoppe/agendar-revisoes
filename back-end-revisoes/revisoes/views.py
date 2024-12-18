21c6f3b04243d071b533c2161b348f8e48801852
luanpoppe <luanpoppe@gmail.com>
2024-12-18 10:18:59 -0300

    fix: ordenação revisoes hoje

--- back-end-revisoes/revisoes/views.py
+++ back-end-revisoes/revisoes/views.py
@@ -63,12 +63,14 @@ def updateReview(request, id):
 
 @api_view(["GET"])
 def revisoesHojeView(request):
-  if request.method == "GET":
-    data = formatDate(daysFromToday(0)).split("-")
-    data = datetime(year=int(data[0]), month=int(data[1]), day=int(data[2])).date()
-    revisoes = RevisoesModel.objects.filter(proxima_data__lte=data, ativo=True).order_by('intervalo_revisao')
-    serializer = RevisoesSerializer(revisoes, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        data = formatDate(daysFromToday(0)).split("-")
+        data = datetime(year=int(data[0]), month=int(data[1]), day=int(data[2])).date()
+        revisoes = RevisoesModel.objects.filter(
+            proxima_data__lte=data, ativo=True
+        ).order_by("intervalo_revisao", "nome")
+        serializer = RevisoesSerializer(revisoes, many=True)
+        return Response(serializer.data)
 
 @api_view(["GET", "POST"])
 def pequenasRevisoesView(request):
