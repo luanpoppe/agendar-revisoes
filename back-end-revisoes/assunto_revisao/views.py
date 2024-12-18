5a6ad4bf42daefbdc312ae7849d20faa0190be87
luanpoppe <luanpoppe@gmail.com>
2024-12-18 09:49:07 -0300

    fix: ordenação das reviews do dia

--- back-end-revisoes/assunto_revisao/views.py
+++ back-end-revisoes/assunto_revisao/views.py
@@ -100,11 +100,9 @@ def revisoesHojeView(request):
     if request.method == "GET":
         data = formatDate(daysFromToday(0)).split("-")
         data = datetime(year=int(data[0]), month=int(data[1]), day=int(data[2])).date()
-        revisoes = (
-            AssuntoRevisaoModel.objects.filter(proxima_data__lte=data, ativo=True)
-            .order_by("intervalo_revisao")
-            .order_by("nome")
-        )
+        revisoes = AssuntoRevisaoModel.objects.filter(
+            proxima_data__lte=data, ativo=True
+        ).order_by("intervalo_revisao", "nome")
         serializer = AssuntosSerializer(revisoes, many=True)
         return Response(serializer.data)
 
